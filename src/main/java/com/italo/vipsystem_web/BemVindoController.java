78fc09cc98ae15636052f4dda3c6d99abe2c62e1
Italo Rocha <italorochha00@gmail.com>
2026-03-31 21:02:56 -0300

    Liberando CORS para acesso do Front-end

--- src/main/java/com/italo/vipsystem_web/BemVindoController.java
+++ src/main/java/com/italo/vipsystem_web/BemVindoController.java
@@ -2,6 +2,7 @@ package com.italo.vipsystem_web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+@CrossOrigin(origins = "*")
 @RestController
 public class BemVindoController {
     @Autowired
